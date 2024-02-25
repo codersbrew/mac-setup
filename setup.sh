4a9c6cd9217cbed616b87848c5a59b37dd701a82
Matt Essenburg <matthew@clan.net>
2024-02-24 20:27:19 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,72 +1,79 @@
 #!/bin/bash
 
-#install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#install homebrew and xcode tools
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew cask install google-chrome
-brew cask install slack
+#install some must haves
+brew install --cask google-chrome
+brew install --cask discord
+brew install --cask google-cloud-sdk
+brew install --cask iterm2
+bew install zsh
 
-brew tap caskroom/versions
-brew cask install java8
+#Setup Terminal - https://catalins.tech/improve-mac-terminal/
+
+#install git
+git config --global user.name "Matthew Essenburg"
+git config --global user.email xxx@essenburg.com
+ssh-keygen -t ed25519 -C "xxx@essenburg.com"
+
+
+#
+#[alias]
+#  st = status
+#  ci = commit
+#  br = branch
+#  co = checkout
+#
+#brew tap caskroom/versions
+#brew cask install java8
 
 #
 # Setup Maven
 # 
 
-brew install maven
-mv ~/.m2/settings.xml ~/.m2/settings.xml.bak
-touch ~/.m2/settings.xml
-echo '<?xml version="1.0" encoding="UTF-8"?>
-<settings xmlns="http://maven.apache.org/SETTINGS/1.1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.1.0 http://maven.apache.org/xsd/settings-1.1.0.xsd">
-  <mirrors>
-    <mirror>
-      <id>adjuster-releases</id>
-      <url>http://gitlab.ad-juster.xyz:8081/artifactory/libs-release</url>
-      <mirrorOf>*</mirrorOf>
-    </mirror>
-  </mirrors>
+#brew install maven
+#mv ~/.m2/settings.xml ~/.m2/settings.xml.bak
+#touch ~/.m2/settings.xml
+#echo '<?xml version="1.0" encoding="UTF-8"?>
+#<settings xmlns="http://maven.apache.org/SETTINGS/1.1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.1.0 http://maven.apache.org/xsd/settings-1.1.0.xsd">
+#  <mirrors>
+#    <mirror>
+#      <id>adjuster-releases</id>
+#      <url>http://gitlab.ad-juster.xyz:8081/artifactory/libs-release</url>
+#      <mirrorOf>*</mirrorOf>
+#    </mirror>
+#  </mirrors>
 
-<!--   <servers>
-    <server>
-      <id>adjuster-releases</id>
-      <username>{your name}</username>
-      <password>{your encrypted pwd}</password>
-    </server>
-    <server>
-      <id>adjuster-snapshots</id>
-      <username>{your name}</username>
-      <password>{your encrypted pwd}</password>
-    </server>
-  </servers> -->
-</settings>' >> ~/.m2/settings.xml
-
-#
-# setup git
-#
-brew install git
-git config --global user.name "Peter Fox"
-git config --global user.email peter.fox@somewhere.com
-
-#  Configure GitLab
-ssh-keygen -t rsa -C "peter.fox@somewhere.com" -b 4096
-pbcopy < ~/.ssh/id_rsa.pub
-# add key to your gitlab: settings -> ssh keys
+#<!--   <servers>
+#    <server>
+#      <id>adjuster-releases</id>
+#      <username>{your name}</username>
+#      <password>{your encrypted pwd}</password>
+#    </server>
+#    <server>
+#      <id>adjuster-snapshots</id>
+#      <username>{your name}</username>
+#      <password>{your encrypted pwd}</password>
+#    </server>
+#  </servers> -->
+#</settings>' >> ~/.m2/settings.xml
 
 #Install IntelliJ
-brew cask install intellij-idea-ce
+#brew cask install intellij-idea-ce
 
-#Install PyCharm
-brew cask install pycharm-ce
+#Install Webstorm
+brew install --cask webstorm
 
 #Install Visual Studio Code
-brew cask install visual-studio-code
+brew install --cask visual-studio
 
 #
 # Install MySQL
 #
-brew install mysql@5.7
-brew services start mysql
-mysql_secure_installation
+#brew install mysql@5.7
+#brew services start mysql
+#mysql_secure_installation
 
 
 #
@@ -76,7 +83,7 @@ brew cask install zsh zsh-completions
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 #https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md#oh-my-zsh
-brew cask install caffeine
-brew cask install licecap
-brew cask install sublime-text
-brew cask install textmate
+#brew cask install caffeine
+#brew cask install licecap
+#brew cask install sublime-text
+#brew cask install textmate
